231b21a8aacbf31d24434ef847ffaaf4ba0e65a8
Maksim Poleshchuk <maksimpoleshchuk@mail.ru>
2021-03-14 00:04:37 +0300

    refactor

--- src/Loader.js
+++ src/Loader.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function Loader() {
-  <div style={{ display: 'flex', justifyContent: 'center', margin: '.5rem' }}>
-    <div className='lds-dual-ring' />
-  </div>
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', margin: '.5rem' }}>
+      <div className='lds-dual-ring' />
+    </div>
+  )
 }
 
 export default Loader;
